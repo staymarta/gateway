fa41125fce6cc2f2bfdee41f70f117c1062f1ccb
Jared Allard <jared@staymarta.com>
2017-04-27 22:30:13 -0700

    router: massive optimization
    Removed circuit breaker in favor of a microservice later
    Optimized code to be less complex, more comments and better.

--- src/lib/router.js
+++ src/lib/router.js
@@ -8,48 +8,7 @@
 
 const debug          = require('./logger.js')('staymarta:router')
 const Communication  = require('libcommunication');
-
 const communication  = new Communication();
-
-const circuitBreaker  = {
-  max_errors: 50,
-  schedule: 10000,
-  min_downtime: 20000,
-  services: {}
-}
-
-const watchdog = () => {
-
-  debug('watchdog running')
-  Object.keys(circuitBreaker.services).forEach(serv => {
-    let service = circuitBreaker.services[serv];
-
-    debug('watchdog', serv);
-
-    // Check for error failure to close.
-    if(service.errors > circuitBreaker.max_errors && service.status === 'OPEN') {
-      debug('watchdog', 'closed', serv, 'with', service.errors)
-      service.status = 'CLOSED';
-      service.closed_at = Date.now();
-    }
-
-    if(service.closed_at && service.status === 'CLOSED'){
-      let now = Date.now()
-      let passed = (now - service.closed_at);
-
-      // if over min, open circuit.
-      if(passed > circuitBreaker.min_downtime) {
-        debug('watchdog', `open '${serv}' after ${passed}ms`);
-        service.errors = 0;
-        service.status = 'OPEN';
-        service.closed_at = 0;
-      }
-    }
-  })
-}
-
-// Check service error rates
-setInterval(watchdog, circuitBreaker.schedule)
 
 communication.connect()
 .then(() => {
@@ -57,22 +16,12 @@ communication.connect()
 })
 
 /**
- * Track errors for ciruit breaker.
- **/
-let error = service => {
-  const circuit = circuitBreaker.services[service]
-  if(!circuit) return debug('notice', 'error on', service, 'but no circuit?')
-
-  // up the error.
-  debug('watchdog:error', 'up error count')
-  circuit.errors++;
-
-  // Close if errors over margin
-  if(circuit.errors > circuitBreaker.max_errors) {
-    debug('watchdog:error', 'closed', service, 'with', circuit.errors)
-    circuit.status = 'CLOSED';
-    circuit.closed_at = Date.now();
-  }
+ * Track service errors for the circuit breaker.
+ * @return {Integer}        0, for compat until implemented.
+ */
+const error = () => {
+  debug('error', 'reporting not implemented.')
+  return 0
 }
 
 /**
@@ -85,41 +34,27 @@ let error = service => {
  * @returns {*} next value.
  **/
 let router = (req, res) => {
-  let commandString = null;
-  let splitPath = req.url.split('/')
+  let commandString;
+  const splitPath = req.url.split('/')
+  const version   = splitPath[1];
+  const service   = splitPath[2];
+  const method    = req.method.toLowerCase();
 
-  if(!splitPath[2]|| splitPath.length <= 2) return res.error('Invalid Path')
-
-  let version = splitPath[1];
-  let service = splitPath[2];
-  let method  = req.method.toLowerCase();
+  // Check if it's even a valid route.
+  if(splitPath.length <= 2) {
+    return res.error('Invalid Path')
+  }
 
   // Generate message string.
   let rmqString = `${version}.${service}.${method}`;
-  let id = req.id;
-
-  let circuit = circuitBreaker.services[service];
-  if(!circuitBreaker.services[service]) {
-    debug('found service', service, `-> /${version}/${service} set OPEN`)
-    circuit = circuitBreaker.services[service] = {
-      status: 'OPEN',
-      endpoint: `/${version}/${service}`,
-      errors: 0
-    }
-  }
-
-  // Check if switch is hit.
-  if(circuit.status !== 'OPEN') {
-    return res.error('Service is down.', 501)
-  }
+  let id        = req.id;
 
   debug('request', id)
 
-  // Check if we have any input her.
+  // Check if we have any input here.
   if(splitPath.length >= 3) {
-    commandString = splitPath[3];
+    commandString = splitPath[3]
   }
-
 
   // Send the message and await the response.
   communication.sendAndWait(rmqString, {
@@ -129,45 +64,56 @@ let router = (req, res) => {
         string: commandString
       }
   })
+
+  // Handle service data.
   .then(data => {
-    const metadata = {
-      info: {
-        method: method,
-        version: version,
-        service: service
-      },
-      rabbitmq: {
-        type: rmqString
-      },
-      gateway: {
-        id: communication.service_id,
-        time: Date.now()
-      }
-    }, serviceRes = data.body.data;
+    const metadata   =
+          {
+            info: {
+              method: method,
+              version: version,
+              service: service
+            },
+            rabbitmq: {
+              type: rmqString
+            },
+            gateway: {
+              id: communication.service_id,
+              time: Date.now()
+            }
+          },
+          body       = data.body,
+          serviceRes = body.data,
+          reqMeta    = body.request,
+          requestId  = reqMeta.id
 
-    debug('message', data.body)
-    if(!data.body.request) console.warn('Message didn\'t have request integreity checking abilities.')
-    let requestId = data.body.request.id;
-    if(!requestId === req.id) console.warn('Recieved request not for us...')
+    debug('message', body)
 
-    metadata.gateway.request_id = data.body.request.id;
-    if(data.body.reply) metadata[service] = data.body.reply;
-
-    // Handle errors.
+    // Handle service reported errors.
     if(serviceRes.error) {
       error(service);
       return res.error(serviceRes.error, serviceRes.code)
     }
 
+    // modify metadata to include request id.
+    metadata.gateway.request_id = requestId
+
+    // if service responded, include that data.
+    if(body.reply) {
+      metadata[service] = body.reply
+    }
+
+    // send a response back...
     return res.send({
       metadata: metadata,
-      [service]: data.body.data || null
+      [service]: serviceRes || null
     })
   })
-  .catch(err => {
-    console.log('error', err)
-    error(service);
+
+  // Handle service timeout.
+  .catch(() => {
     debug('error', id, `couldn\'t reach '${service}' in time.`)
+    error(service);
     return res.error('Failed to retrieve response in allocated time.', 200, 503)
   })
 }
